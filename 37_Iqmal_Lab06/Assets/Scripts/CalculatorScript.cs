9b8fac2719fe0d0d8afc0857ad2383df8f49ffd1
Md.Iqmal <111185577+User101fafdasfsfsa@users.noreply.github.com>
2023-01-17 12:20:37 +0800

    UNITY_LAB06

---
+++ 37_Iqmal_Lab06/Assets/Scripts/CalculatorScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalculatorScript : MonoBehaviour
+{
+    public float Amount;
+    public float SGDRM = 3.27f;
+    public float SGDIDR = 11447.60f;
+
+    public Toggle RM;
+    public Toggle IDR;
+
+    public InputField InputAmount;
+    public InputField InputConverted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RM.isOn = false;
+        IDR.isOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Conversion()
+    {
+        float Amount = float.Parse(InputAmount.text);
+
+        if(RM.isOn == true)
+        {
+            IDR.isOn = false;
+
+            InputConverted.text = "RM " + (Amount * SGDRM);
+        }
+        else if(IDR.isOn == true)
+        {
+            RM.isOn = false;
+
+            InputConverted.text = "Rupiah " + (Amount * SGDIDR);
+        }
+    }
+
+    public void Clear()
+    {
+        RM.isOn = false;
+        IDR.isOn = false;
+
+        InputAmount.text = " ";
+        InputConverted.text = " ";
+    }
+}
